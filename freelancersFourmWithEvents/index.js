92b543ab01c4726bdc69d3a3be94b4951a48323d
Porter Peterson <porterpeterson@Porters-MacBook-Air.local>
2023-12-07 09:44:41 -0700

    first commit

---
+++ freelancersFourmWithEvents/index.js
@@ -0,0 +1,55 @@
+const names = [
+  "Dr. Slice",
+  "Dr. Pressure",
+  "Prof. Possibility",
+  "Prof. Prism",
+  "Dr. Impulse",
+  "Prof. Spark",
+  "Dr. Wire",
+  "Prof. Goose",
+  "MF Doom",
+  "MR Fantastic",
+  "Slim Shady",
+  "Ruby da Cherry",
+  "$crim",
+];
+
+const occupations = [
+  "gardener",
+  "programmer",
+  "teacher",
+  "mechanic",
+  "welder",
+  "dog walker",
+  "rapper",
+  "elf",
+];
+
+const jobsList = document.querySelector("#jobsList");
+
+function generateRandom() {
+  const nameIdx = Math.floor(Math.random() * names.length);
+  const name = names[nameIdx];
+  const occupationIdx = Math.floor(Math.random() * occupations.length);
+  const occupation = occupations[occupationIdx];
+  const price = Math.ceil(Math.random() * 100);
+  return {
+    name,
+    occupation,
+    price,
+  };
+}
+
+function render() {
+  const lis = jobs
+    .map(function (jobs) {
+      return `<li>${jobs.name} is a ${jobs.occupation} and makes $${jobs.price} an hour<li/>`;
+    })
+    .join(" ");
+  console.log(lis);
+  jobsList.innerHTML = lis;
+}
+
+const jobs = [];
+jobs.push(generateRandom());
+render();
